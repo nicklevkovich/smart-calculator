23e4c762faf33139eaf2b7c17505d31a59ad3fe4
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-06 03:07:57 +0300

    Final Solution. 306/306 passing

--- src/index.js
+++ src/index.js
@@ -1,26 +1,80 @@
 class SmartCalculator {
+
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push({
+        num: initialValue,
+        operand: null,
+        priority: 0
+    });
   }
 
   add(number) {
-    // your implementation
+    this.stack.push({
+        num: number,
+        operand: (a, b) => a + b,
+        priority: 2
+    });
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.stack.push({
+        num: -number,
+        operand: (a, b) => a + b,
+        priority: 1
+    });
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push({
+        num: number,
+        operand: (a, b) => a * b,
+        priority: 4
+    });
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push({
+        num: number,
+        operand: (a, b) => a / b,
+        priority: 3
+    });
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push({
+        num: number,
+        operand: (a, b) => a ** b,
+        priority: 5
+    });
+    return this;
+  }
+  valueOf() {
+    let result = 0;
+    if (this.stack.length === 0) result = 0
+    else if (this.stack.length === 1) result = this.stack[0].num
+    else {
+        for (let i = 5; i > 0; i--) {
+            for (let j = this.stack.length - 1; j >= 0; j--) {
+                if (this.stack[j].priority === i) {
+                    let func = this.stack[j].operand;
+                    this.stack[j - 1].num = func(this.stack[j - 1].num, this.stack[j].num);
+                    if (this.stack[j - 1].num > 0 
+                        && this.stack[j - 1].priority === 1 
+                        && this.stack[j].priority === 5) { 
+                        this.stack[j - 1].num = -this.stack[j - 1].num;
+                        }
+                    this.stack.splice(j, 1);
+                }
+            }
+        }
+        result = this.stack[0].num;
+    }
+    return result;
   }
 }
 
